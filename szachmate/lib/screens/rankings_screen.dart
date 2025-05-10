52357063cb384a8124365fa93162121e82e13225
chatlxrdd <londoyt@gmail.com>
2025-05-10 14:05:57 +0200

    done

--- szachmate/lib/screens/rankings_screen.dart
+++ szachmate/lib/screens/rankings_screen.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 import '../data/mock_data.dart'; // Import danych
 
 class RankingsScreen extends StatefulWidget {
+  const RankingsScreen({super.key});
+
   @override
   _RankingsScreenState createState() => _RankingsScreenState();
 }
 
 class _RankingsScreenState extends State<RankingsScreen> {
-  String selectedFilter = 'wszyscy';
-
   @override
   Widget build(BuildContext context) {
     List sortedPlayers = [...mockPlayers];
@@ -20,39 +20,6 @@ class _RankingsScreenState extends State<RankingsScreen> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
-            Row(
-              children: [
-                FilterChip(
-                  label: Text("Wszyscy"),
-                  selected: selectedFilter == 'wszyscy',
-                  onSelected: (_) {
-                    setState(() {
-                      selectedFilter = 'wszyscy';
-                    });
-                  },
-                ),
-                SizedBox(width: 8),
-                FilterChip(
-                  label: Text("Online"),
-                  selected: selectedFilter == 'online',
-                  onSelected: (_) {
-                    setState(() {
-                      selectedFilter = 'online';
-                    });
-                  },
-                ),
-                SizedBox(width: 8),
-                FilterChip(
-                  label: Text("Lokalni"),
-                  selected: selectedFilter == 'lokalni',
-                  onSelected: (_) {
-                    setState(() {
-                      selectedFilter = 'lokalni';
-                    });
-                  },
-                ),
-              ],
-            ),
             SizedBox(height: 16),
             Expanded(
               child: ListView.builder(
@@ -65,12 +32,6 @@ class _RankingsScreenState extends State<RankingsScreen> {
                       title: Text(player['name']),
                       subtitle: Text(
                         "Ranking: ${player['rating']} | Turnieje: ${player['tournaments']}",
-                      ),
-                      trailing: TextButton(
-                        onPressed: () {
-                          // Można rozbudować o szczegóły gracza
-                        },
-                        child: Text("Zobacz"),
                       ),
                     ),
                   );
